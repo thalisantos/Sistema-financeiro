f1fc4ed4a7c673c8a8adeabc1154d839fda05c38
Thalita Santos <mariathalitasantos00@gmail.com>
2022-03-18 09:20:53 -0300

    primeiro commit

---
+++ src/components/NewTransactionModal/style.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.form`
+  h2 {
+    color: var(--text-title);
+    font-size: 2rem;
+    margin-bottom: 5rem;
+  }
+
+  input {
+  width: 100%;
+  padding: 0 1.5rem;
+  height: 4rem;
+  border-radius: 0.25rem;
+
+  border: 1px solid #d7d7d7;
+  background: black;
+
+  font-weight: 400;
+  font-size: 1rem;
+  }
+`;
